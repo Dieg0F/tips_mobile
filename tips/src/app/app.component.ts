67d36126834f759b98b839c75ce88d2edfa0dd9b
Dieg0F <dfraga2012@gmail.com>
2018-12-06 01:04:13 -0200

    Criado as telas de perfil, busca e resultados

--- tips/src/app/app.component.ts
+++ tips/src/app/app.component.ts
@@ -13,7 +13,9 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.backgroundColorByHexString("#273A56");
+      statusBar.styleLightContent();
+
       splashScreen.hide();
     });
   }
--- tips/src/pages/professional/profile/profile.module.ts
+++ tips/src/pages/professional/profile/profile.module.ts
@@ -3,11 +3,7 @@ import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from './profile';
 
 @NgModule({
-  declarations: [
-    ProfilePage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ProfilePage),
-  ],
+  declarations: [ProfilePage],
+  imports: [IonicPageModule.forChild(ProfilePage)]
 })
 export class ProfilePageModule {}
