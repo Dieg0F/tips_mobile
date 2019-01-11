b0805ce34c12c237f27717bf634255495019ac18
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2019-01-10 22:51:13 -0200

    Firebase

--- tips/src/app/app.component.ts
+++ tips/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AngularFireAuth } from '@angular/fire/auth';
+
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -7,10 +9,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = "LoginPage";
+  rootPage:any = null;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, afAuth: AngularFireAuth) {
     platform.ready().then(() => {
+      //Verifica se usuário já está logado
+      afAuth.auth.onAuthStateChanged((_user) => {
+        if(_user != null) { //Logado
+          console.log('logado');
+        } else { //não logado
+          this.rootPage = "LoginPage";
+          console.log('não logado, faça login ou cadastre-se');
+        }
+      })
+
       if ('MobileAccessibility' in window) {
         const { MobileAccessibility }: any = window;
         MobileAccessibility.usePreferredTextZoom(false);
--- tips/src/app/app.module.ts
+++ tips/src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { MyApp } from './app.component';
 
 import { ComponentsModule } from '../components/components.module';
+
+const config = {
+  apiKey: "AIzaSyDlOsZfCTUVq4WdVUQReZaQqUb_6UCJWqo",
+  authDomain: "tips-36c4a.firebaseapp.com",
+  databaseURL: "https://tips-36c4a.firebaseio.com",
+  projectId: "tips-36c4a",
+  storageBucket: "tips-36c4a.appspot.com",
+  messagingSenderId: "1068679955578"};
 
 @NgModule({
   declarations: [
@@ -15,7 +28,11 @@ import { ComponentsModule } from '../components/components.module';
   imports: [
     BrowserModule,
     ComponentsModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
