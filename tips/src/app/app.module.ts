848103d6cb8ba0aa7b717bee77deb5eb2055c8ce
Dieg0f <dfraga2012@gmail.com>
2019-10-19 11:23:48 -0300

    fix some not used data.

--- tips/src/app/app.module.ts
+++ tips/src/app/app.module.ts
@@ -1,21 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { IonicStorageModule } from '@ionic/storage';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-
-import { MyApp } from './app.component';
-
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { Camera } from '@ionic-native/camera';
+import { FCM } from '@ionic-native/fcm';
 import { FileChooser } from '@ionic-native/file-chooser';
+import { Geolocation } from '@ionic-native/geolocation';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { BrMaskerModule } from 'brmasker-ionic-3';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { StarRatingModule } from 'ionic3-star-rating';
 import { ComponentsModule } from '../components/components.module';
 import { AppConfigProvider } from '../providers/app-config/app-config';
 import { AuthProvider } from '../providers/auth/auth';
@@ -23,24 +23,18 @@ import { AvaliationProvider } from '../providers/avaliation/avaliation';
 import { DataProvider } from '../providers/data/data';
 import { Locations } from '../providers/locations/locations';
 import { ProfileProvider } from '../providers/profile/profile';
+import { SectorProvider } from '../providers/sector/sector';
+import { SolicitationProvider } from '../providers/solicitations/solicitations';
 import { StorageProvider } from '../providers/storage/storage';
 import { UserProvider } from '../providers/user/user';
 import { Alert } from '../util/alert/alert';
 import { CameraProvider } from '../util/camera/camera';
 import { HttpService } from '../util/http/http';
 import { Loading } from '../util/loading/loading';
-import { Toast } from '../util/toast/toast';
-
-import { Geolocation } from '@ionic-native/geolocation';
-import { StarRatingModule } from 'ionic3-star-rating';
-import { AreaProvider } from '../providers/area/area';
-import { SectorProvider } from '../providers/sector/sector';
 import { Notifications } from '../util/notifications/notifications';
 import { Popover } from '../util/popover/popover';
-
-import { FCM } from '@ionic-native/fcm';
-import { BrMaskerModule } from 'brmasker-ionic-3';
-import { SolicitationProvider } from '../providers/solicitations/solicitations';
+import { Toast } from '../util/toast/toast';
+import { MyApp } from './app.component';
 
 const config = {
   apiKey: 'AIzaSyBUzDf7u-UXxfNLch_ucKZTxo9pfsXgxpc',
@@ -88,7 +82,6 @@ const config = {
     CameraProvider,
     SolicitationProvider,
     SectorProvider,
-    AreaProvider,
     DataProvider,
     HttpService,
     FileChooser,
